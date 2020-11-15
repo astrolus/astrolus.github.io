a0b431cfa652426c1302feeb1c7484224861c367
Josh <astrolus@users.noreply.github.com>
2020-11-14 22:19:23 -0600

    modified:   aboutme.html 	new file:   css/appearance.js 	modified:   css/styles.css 	modified:   education.html 	deleted:    images/IMG_0193.jpg 	deleted:    images/IMG_0388.jpg 	deleted:    images/IMG_0434.HEIC 	deleted:    images/IMG_0798.jpg 	deleted:    images/IMG_2011.JPG 	renamed:    images/20191109_125105_HDR.jpg -> images/carousel-images/20191109_125105_HDR - Copy.jpg 	new file:   images/carousel-images/IMG_0388.jpg 	new file:   images/carousel-images/IMG_0798.jpg 	new file:   images/carousel-images/IMG_1828.jpg 	new file:   images/carousel-images/IMG_1981.jpg 	new file:   images/carousel-images/IMG_2944.jpg 	new file:   images/carousel-images/IMG_8535 - Copy.JPG 	modified:   index.html 	modified:   workexperience.html

---
+++ css/appearance.js
@@ -0,0 +1,10 @@
+
+
+// Dark Mode Function
+let lightModeBackgroundColor = '#dfdfdf'
+let darkModeBackgroundColor = '#343434'
+
+function darkAndLightMode()
+{
+    if(document.querySelector('body'))
+}
